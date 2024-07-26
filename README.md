3d990a99477ea1528177681b5ca7c3b833f1153f
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-07-25 23:32:41 -0500

    Update comments for config settings and begin work on README.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Public-Ender-Chest
-A Minecraft server-side mod that adds a shared ender chest inventory accessible via an ender chest.
+<h1 align="center">
+    <img src="https://i.imgur.com/SxdRC9A.png">
+</h1>
+
+
+# Public Ender Chest
+This server-side, configurable Fabric Minecraft mod adds a new public inventory accessible via Ender Chest blocks.
+The mod also includes a built-in logging system for server admins to keep track of changes to this public inventory, and some other useful configuration options.
+
+## Features
+
+### Configuration 
--- src/main/kotlin/xd/arkosammy/publicenderchest/config/ConfigSettings.kt
+++ src/main/kotlin/xd/arkosammy/publicenderchest/config/ConfigSettings.kt
@@ -8,21 +8,21 @@ import xd.arkosammy.monkeyconfig.util.SettingLocation
 
 enum class ConfigSettings(private val builder: ConfigSetting.Builder<*, *, *>) {
     ENABLE_PLAYER_BLACKLIST(BooleanSetting.Builder(SettingLocation(SettingGroups.PLAYER_BLACKLIST.groupName, "enable_player_blacklist"), """
-        Toggle the use of the player blacklist feature.
+        (Default = false) Toggle the use of the player blacklist feature.
     """.trimIndent(), false)),
     PLAYER_BLACKLIST(StringListSetting.Builder(SettingLocation(SettingGroups.PLAYER_BLACKLIST.groupName, "player_blacklist"), """
         Add players to this blacklist by specifying their username.
     """.trimIndent(), mutableListOf())),
 
     ENABLE_DIMENSION_BLACKLIST(BooleanSetting.Builder(SettingLocation(SettingGroups.DIMENSION_BLACKLIST.groupName, "enable_dimension_blacklist"), """
-        Toggle the use of the dimension blacklist feature.
+        (Default = false) Toggle the use of the dimension blacklist feature.
     """.trimIndent(), false)),
     DIMENSION_BLACKLIST(StringListSetting.Builder(SettingLocation(SettingGroups.DIMENSION_BLACKLIST.groupName, "dimension_blacklist"), """
         Add dimensions to this blacklist by specifying their full identifier. For example, to add the Overworld dimension, you can add the identifier "minecraft:overworld" to this list.
     """.trimIndent(), mutableListOf())),
 
     PURGE_OLDER_THAN_X_DAYS(NumberSetting.Builder(SettingLocation(SettingGroups.DATABASE.groupName, "purge_older_than_x_days"), """
-        Purge database entries older than the amount of days specified. The database is purged on every server shutdown and using the `/publicenderchest database purge` command.
+        (Default = 30) Purge database entries older than the amount of days specified. The database is purged on every server shutdown and using the `/publicenderchest database purge` command.
     """.trimIndent(), 30).withLowerBound(0));
 
     val settingLocation: SettingLocation = builder.settingLocation
